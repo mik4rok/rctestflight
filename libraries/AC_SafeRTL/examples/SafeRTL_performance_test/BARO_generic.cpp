d197c4f09304528d475983cc33f01d9be8e9bb40
squilter <squilter@mit.edu>
2017-06-21 15:14:15 -0400

    strip example code down to bare minimum

--- libraries/AC_SafeRTL/examples/SafeRTL_performance_test/BARO_generic.cpp
+++ libraries/AC_SafeRTL/examples/SafeRTL_performance_test/BARO_generic.cpp
@@ -2,32 +2,22 @@
   generic Baro driver test
  */
 
-#include <AP_Baro/AP_Baro.h>
+#include <AC_SafeRTL/AC_SafeRTL.h>
 #include <AP_BoardConfig/AP_BoardConfig.h>
 #include <AP_HAL/AP_HAL.h>
 
 const AP_HAL::HAL &hal = AP_HAL::get_HAL();
-
-static AP_Baro barometer;
-
-static uint32_t timer;
-static uint8_t counter;
 
 void setup();
 void loop();
 
 void setup()
 {
-    hal.console->printf("Barometer library test\n");
+    hal.console->printf("SafeRTL performance test\n");
 
     AP_BoardConfig{}.init();
 
     hal.scheduler->delay(1000);
-
-    barometer.init();
-    barometer.calibrate();
-
-    timer = AP_HAL::micros();
 }
 
 void loop()
@@ -36,34 +26,14 @@ void loop()
         return;
     }
 
-    // run accumulate() at 50Hz and update() at 10Hz
-    if ((AP_HAL::micros() - timer) > 20 * 1000UL) {
-        timer = AP_HAL::micros();
-        barometer.accumulate();
-        if (counter++ < 5) {
-            return;
-        }
-        counter = 0;
-        barometer.update();
-        uint32_t read_time = AP_HAL::micros() - timer;
-        if (!barometer.healthy()) {
-            hal.console->printf("not healthy\n");
-            return;
-        }
-        hal.console->printf(" Pressure: %.2f Pa\n"
-                            " Temperature: %.2f degC\n"
-                            " Relative Altitude: %.2f m\n"
-                            " climb=%.2f m/s\n"
-                            " Read + update time: %u usec\n"
-                            "\n",
-                            (double)barometer.get_pressure(),
-                            (double)barometer.get_temperature(),
-                            (double)barometer.get_altitude(),
-                            (double)barometer.get_climb_rate(),
-                            read_time);
-    } else {
-        hal.scheduler->delay(1);
-    }
+    uint32_t reference_time = AP_HAL::micros();
+
+    hal.scheduler->delay(420); // TODO run something useful here.
+
+    uint32_t run_time = AP_HAL::micros() - reference_time;
+    hal.console->printf("Cleanup time: %u usec\n", run_time);
+
+    hal.scheduler->delay(5000);
 }
 
 AP_HAL_MAIN();
