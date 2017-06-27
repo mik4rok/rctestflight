22759c9e2534f690db7379972078dcbe9178aac3
squilter <squilter@mit.edu>
2017-06-27 17:48:13 -0400

    implement _detect_loops()

--- libraries/AC_SafeRTL/AC_SafeRTL.cpp
+++ libraries/AC_SafeRTL/AC_SafeRTL.cpp
@@ -25,50 +25,38 @@
 *    will just have to wait for a bit until they are both done.
 */
 
-RDP_Stack::RDP_Stack()
-{
-    stack = new start_finish[RDP_STACK_LEN];
-    top = stack;
-}
-
-void RDP_Stack::push(start_finish item)
-{
-    *(++top) = item;
-}
-
-start_finish RDP_Stack::pop()
-{
-    return *(top--);
-}
-
-bool RDP_Stack::empty()
-{
-    return stack == top;
-}
-
 // TODO deal with issues that might arise from multithreading
 Path::Path() :
-    _last_index(0),
+    __last_index(0),
     _simplification_complete(False),
     _pruning_complete(False),
     _pruning_current_i(0),
-    _pruning_max_j(0)
+    _pruning_min_j(0)
 {
     _simplification_bitmask = std::bitset<MAX_PATH_LEN>().set(); //initialize to 1111...
-    path = new Vector3f[MAX_PATH_LEN];
-    _simplification_stack = new RDP_Stack();
 }
 
 void Path::append_if_far_enough(Vector3f p) {
-    if (HYPOT(p, path[_last_index]) > POSITION_DELTA) {
-        path[_last_index++] = p;
+    if (HYPOT(p, path[__last_index]) > POSITION_DELTA) {
+        path[__last_index++] = p;
+        // if cleanup algorithms are finished (And therefore not runnning), reset them
+        if (_pruning_complete) {
+            _pruning_complete = false;
+            _pruning_current_i = 0;
+            _pruning_max_j = 0;
+        }
+        if (_simplification_complete) {
+            _simplification_complete = false;
+            _simplification_bitmask.set();
+            _simplification_stack.clear();
+        }
     }
 }
 
 void Path::routine_cleanup() {
     // We only do a routine cleanup if the memory is almost full. Cleanup deletes
     // points which are potentially useful, so it would be bad to clean up if we don't have to
-    if (_last_index < MAX_PATH_LEN - 10) {
+    if (__last_index < MAX_PATH_LEN - 10) {
         return;
     }
 
@@ -115,14 +103,13 @@ void Path::thorough_cleanup() {
 *    This function has been tested. It produces the correct output.
 *    TODO this function should return a bool, and leave a bitset in memory
 */
-int Path::_rdp(uint8_t end_index, float epsilon)
-{
+int Path::_rdp(float epsilon, uint32_t allowed_microseconds) {
     uint8_t start_index;
     uint8_t end_of_array = end_index;
     start_finish sf = {0, end_index};
-    _simplification_stack->push(sf);
-    while (!_simplification_stack->empty()) {
-        start_finish tmp = _simplification_stack->pop();
+    _simplification_stack.push_back(sf);
+    while (!_simplification_stack.is_empty()) {
+        start_finish tmp = _simplification_stack.pop_front();
         start_index = tmp.start;
         end_index = tmp.finish;
 
@@ -139,8 +126,8 @@ int Path::_rdp(uint8_t end_index, float epsilon)
         }
 
         if (max_dist > epsilon) {
-            _simplification_stack->push(start_finish{start_index, index});
-            _simplification_stack->push(start_finish{index, end_index});
+            _simplification_stack.push_back(start_finish{start_index, index});
+            _simplification_stack.push_back(start_finish{index, end_index});
         }
         else {
             for (int i = start_index + 1; i < end_index; i++) {
@@ -151,30 +138,49 @@ int Path::_rdp(uint8_t end_index, float epsilon)
     return 0;
 }
 
-// TODO make this an anytime algorithm. Make is accept a time to run before pausing.
-// then save the state somewhere. What to return? The state, a finished bool?
-bool Path::_detect_loops() {
+/**
+*   This method runs for the allotted time, and detects loops in a path. All detected loops are added to _prunable_loops,
+*   this function does not alter the path in memory.
+*
+*   Note that this method might take a bit longer than allowed_microseconds. It only stops after it's already run longer than allowed_microseconds.
+*/
+void Path::_detect_loops(uint32_t allowed_microseconds) {
+    if (_pruning_complete){
+        return;
+    }
     // pruning step
-    bool pruning_occured = FALSE;
-    for (int i = 0; i <= _last_index; i++) {
-        for (int j = _last_index - 1; j > i + 1; j--) {
+    uint32_t start_time = AP_HAL::micros();
+
+    while (_pruning_current_i < _last_index - 1){
+        if (AP_HAL::micros() - start_time > allowed_microseconds) {
+            return;
+        }
+
+        int j = _pruning_current_i + 2;
+        if (_pruning_min_j > j){
+            j = _pruning_min_j;
+        }
+        while (j < _last_index){
             dist_point dp = _segment_segment_dist(path[i], path[i+1], path[j], path[j+1]);
             if (dp.distance <= PRUNING_DELTA) {
-                // TODO return a struct (to-be-defined)
-                pruning_occured = true;
+                _pruning_min_j = j;
+                _prunable_loops.push_back({i+1,j+1,dp.point});
             }
+            j++;
         }
+        _pruning_current_i++;
     }
+    _pruning_complete = true;
 }
 
 /**
 * Removes all (0,0,0) points from the path, and shifts remaining items to correct position.
 */
-int Path::_remove_empty_points() {
+void Path::_remove_empty_points() {
     int i = 0;
     int j = 0;
     int removed = 0;
-    while (++i < _last_index) { // never removes the first item. This should obviously be {0,0,0}
+    while (++i < __last_index) { // never removes the first item. This should obviously be {0,0,0}
         // if (path[i] == Vector3f(0.0f, 0.0f, 0.0f)) { // does this comparison work?
         if (path[i] == NULL){
             path[++j] = path[i]
@@ -182,7 +188,7 @@ int Path::_remove_empty_points() {
             removed++;
         }
     }
-    _last_index -= removed;
+    __last_index -= removed;
 }
 
 void Path::_zero_points_by_bitmask() {
@@ -201,7 +207,7 @@ void Path::_zero_points_by_bitmask() {
 *  Limitation: This function does not work for parallel lines. In this case, it will return FLT_MAX. This does not matter for the path cleanup algorithm because
 *  the pruning will still occur fine between the first parallel segment and a segment which is directly before or after the second segment.
 */
-dist_point Path::_segment_segment_dist(Vector3f p1, Vector3f p2, Vector3f p3, Vector3f p4) {
+dist_point Path::_segment_segment_dist(const Vector3f& p1, const Vector3f& p2, const Vector3f& p3, const Vector3f& p4) {
     Vector3f u = p2-p1;
     Vector3f v = p4-p3;
     Vector3f w = p1-p3;
@@ -238,7 +244,7 @@ dist_point Path::_segment_segment_dist(Vector3f p1, Vector3f p2, Vector3f p3, Ve
 /**
 *  Returns the closest distance from a point to a 3D line. The line is defined by any 2 points
 */
-float Path::_point_line_dist(Vector3f point, Vector3f line1, Vector3f line2) {
+float Path::_point_line_dist(const Vector3f& point, const Vector3f& line1, const Vector3f& line2) {
     // triangle side lengths
     float a = HYPOT(point, line1);
     float b = HYPOT(line1, line2);
--- libraries/AC_SafeRTL/AC_SafeRTL.h
+++ libraries/AC_SafeRTL/AC_SafeRTL.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <AP_Buffer/AP_Buffer.h>
+#include <AP_HAL/AP_HAL.h>
 #include <AP_Math/AP_Math.h>
-#include <bitset> // TODO if this uses a non-negligible amount of memory, reimplement without this
 
-#define SMALL_FLOAT  0.0000001
+#include <bitset> // TODO if this uses a non-negligible amount of memory, re-implement without this
+#include <vector>
+
+#define SMALL_FLOAT  0.0000001f
 
 #define POSITION_DELTA 2.0f // how many meters to move before appending a new position to return_path
 #define PRUNING_DELTA (POSITION_DELTA * 1.5) //h ow many meteres apart must two points be, such that we can assume that there is no obstacle between those points
@@ -13,35 +17,14 @@
 // XXX A number too small for RDP_STACK_LEN can cause a buffer overflow! The number to put here is int((s/2-1)+min(s/2, MAX_PATH_LEN-s)), where s = pow(2, floor(log(MAX_PATH_LEN)/log(2)))
 // To avoid this annoying math, a good-enough overestimate is ciel(MAX_PATH_LEN*2./3.)
 
-// dot product is already defined using the * operator with Vector3f objects
-// cross product is %. length() also exists
-
 #define HYPOT(a,b) (a-b).length()
 
-typedef struct start_finish {
-    uint8_t start;
-    uint8_t finish;
-} start_finish;
-
-class RDP_Stack { // TODO re-implement using AP_Buffer
-    start_finish * stack;
-    start_finish * top;
-public:
-    RDP_Stack();
-    void push(start_finish);
-    start_finish pop();
-    bool empty();
-};
-
-typedef struct dist_point {
-    float distance;
-    Vector3f point;
-} dist_point;
+const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
 class Path {
     // points are stored in meters from EKF origin in NED
-    Vector3f* path; // by leaving this public, we can inject data into here easily which helps to benchmark algorithm performance. FIXME move it back.
-    int last_index;
+    Vector3f path[MAX_PATH_LEN];
+    int _last_index;
 public:
     Path();
     void append_if_far_enough(Vector3f);
@@ -50,16 +33,35 @@ public:
 private:
     int _rdp(uint8_t, float);
     bool _detect_loops();
+    // _segment_segment_dist returns two things, the closest distance reached between 2 line segments, and the point exactly between them.
+    typedef struct dist_point {
+        float distance;
+        Vector3f point;
+    } dist_point;
     static dist_point _segment_segment_dist(Vector3f, Vector3f, Vector3f, Vector3f);
     static float _point_line_dist(Vector3f, Vector3f, Vector3f);
 
     // Simplification state
     bool _simplification_complete;
+    // structure and buffer to hold the "to-do list" for the RDP algorithm.
+    struct start_finish {
+        uint8_t start;
+        uint8_t finish;
+    };
+    AP_Buffer<start_finish,RDP_STACK_LEN> _simplification_stack;
+    // the result of the simplification algorithm
     std::bitset<MAX_PATH_LEN>() _simplification_bitmask;
-    RDP_Stack* _simplification_stack;
 
     // Pruning state
     bool _pruning_complete;
     int _pruning_current_i;
-    int _pruning_max_j;
+    int _pruning_min_j;
+
+    typedef struct loop {
+        uint8_t start_index;
+        uint8_t end_index;
+        Vector3f halfway_point;
+    } loop;
+    // the result of the pruning algorithm
+    std::vector<loop> _prunable_loops; // TODO this might allocate memory while flying. Maybe consider a way to do this with an array or AP_Buffer
 };
